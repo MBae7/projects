140b334be5576795d787c59eb37534d05b4c43e7
MBae7 <143737119+MBae7@users.noreply.github.com>
2024-09-26 10:28:20 -0700

    fixed resize added arrows

--- maze1.js
+++ maze1.js
@@ -1,3 +1,12 @@
+let left,right,up, down;
+
+function preload(){
+    left = loadImage('left.png');
+    right = loadImage('right.png');
+    up = loadImage('up.png');
+    down = loadImage('down.png');
+}
+
 function setup(){
      createCanvas(windowWidth, windowHeight);
     //grid
@@ -5,21 +14,32 @@ function setup(){
     let sx;
     let sy;
     //buttons
-    let bBarH;
-    let bx,by,bw,bh,bn;
+    let bBarH=windowHeight/8;;
+    let bx,by,bw,bn;
+    let bh=bBarH/2.1;
     let ux,dx,lx,rx;
+    let ax=0;
+    let aw=bh;
+    
+    
+    left.resize(aw, aw);
+    right.resize(aw, aw);
+    up.resize(aw, aw);
+    down.resize(aw, aw);
+
     
 }
 
+
+
 function draw(){ 
-     bBarH=windowHeight/5;
      bBarH=windowHeight/8;
 //grid 
    
     if(windowWidth<windowHeight){
         totalSquareW = windowWidth-bBarH;
     }else{
-        totalSquareW= windowHeight=bBarH;
+        totalSquareW= windowHeight-bBarH;
     }
     
   
@@ -37,17 +57,24 @@ function draw(){
     by=0;
     bw=windowWidth/bn*.8;
     bh=bBarH/2.1;
-    rect(bx,by,bw,bh);
     lx=0;
-    ax=totalSquareW/5;
+    ux=totalSquareW/5;
     dx=2*totalSquareW/5;
-    rx=4*totalSquareW/5;
+    rx=3*totalSquareW/5;
+    
     
     //boxes 
     rect(lx,by,bw,bh);
-    rect(ax,by,bw,bh);
+    image(left, lx, by);
+    rect(ux,by,bw,bh);
+    image(up, ux, by);
     rect(dx,by,bw,bh);
+    image(down, dx, by);
     rect(rx,by,bw,bh);
+    image(right, rx, by);
+    
+    aw = bh;
+   
   
     
     
@@ -58,3 +85,29 @@ function windowResized() {
     resizeCanvas(windowWidth, windowHeight); 
 }
 
+function mousePressed(){
+     //collision detection
+    if(mouseY>by&&mouseY<by+bh){
+        if(mouseX>ux &&mouseX<ux+bw){
+            //up
+            print('up pressed');
+            image(up, ax, by);
+            ax+=aw;
+        }else if(mouseX>lx &&mouseX<lx+bw){
+            //left
+            print('left pressed');
+            ax+=aw;
+        }else if(mouseX>dx &&mouseX<dx+bw){
+            //down
+            print('down pressed');
+            ax+=aw;
+        }else if(mouseX>rx &&mouseX<rx+bw){
+            //right
+            print('right pressed');
+            ax+=aw;
+        }
+    }
+}
+
+//python3 -m http.server
+//http://localhost:8000/
