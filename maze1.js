d3e105c48a6aaa9217554943f41925cd7cd32296
MBae7 <143737119+MBae7@users.noreply.github.com>
2024-10-23 11:45:35 -0700

    somewhat working but skipping first index

--- maze1.js
+++ maze1.js
@@ -1,40 +1,50 @@
 let left,right,up, down;
+ let mx=0;
+ let ms=40;
+ let order='';
+let mxv=1;
+let myv=1;
+ let ax;
+  //grid
+    let totalSquareW;
+    let sx;
+    let sy;
+    //buttons
+    let bBarH;
+    let bx,by,bw,bn;
+    let bh=bBarH/2.1;
+let my;
+
+    let ux,dx,lx,rx,gx;
+     ax=0;
+    let aw;
+    let ss;
+    let go = false; 
+    let index = 0;
+    let i;
 
 function preload(){
     left = loadImage('left.png');
     right = loadImage('right.png');
     up = loadImage('up.png');
     down = loadImage('down.png');
-   let ax;
+  
+       
+   
 }
-
 function setup(){
-     createCanvas(windowWidth, windowHeight);
-    //grid
-    let totalSquareW;
-    let sx;
-    let sy;
-    //buttons
-    let bBarH=windowHeight/8;;
-    let bx,by,bw,bn;
-    let bh=bBarH/2.1;
-    let ux,dx,lx,rx;
-     ax=0;
-    let aw=bh;
-    
-    
-    left.resize(aw, aw);
-    right.resize(aw, aw);
-    up.resize(aw, aw);
-    down.resize(aw, aw);
-
-    
+     createCanvas(windowWidth, windowHeight);   
 }
 
 
 
 function draw(){ 
      bBarH=windowHeight/8;
+   if(go==false){
+       my=bBarH;
+   }
+   
+    
 //grid 
    
     if(windowWidth<windowHeight){
@@ -44,7 +54,7 @@ function draw(){
     }
     
   
-    let ss=totalSquareW/5;
+     ss=totalSquareW/5;
     
     for(let i =0; i<5; i++){
         sx=i*ss;
@@ -59,22 +69,12 @@ function draw(){
     bw=windowWidth/bn*.8;
     bh=bBarH/2.1;
     lx=0;
-    ux=totalSquareW/5;
-    dx=2*totalSquareW/5;
-    rx=3*totalSquareW/5;
+    ux=bw;
+    dx=2*bw;
+    rx=3*bw;
+    gx=4*bw;
     
-    
-    //boxes 
-    rect(lx,by,bw,bh);
-    image(left, lx, by);
-    rect(ux,by,bw,bh);
-    image(up, ux, by);
-    rect(dx,by,bw,bh);
-    image(down, dx, by);
-    rect(rx,by,bw,bh);
-    image(right, rx, by);
-    
-    aw = bh;
+    aw=bh;
    
   
     left.resize(aw, aw);
@@ -82,6 +82,37 @@ function draw(){
     up.resize(aw, aw);
     down.resize(aw, aw);
     
+    //boxes 
+    fill('#FFE9D0');
+    rect(lx,by,bw,bh);
+    image(left, lx, by);
+    fill('#FFFED3');
+    rect(ux,by,bw,bh);
+    image(up, ux, by);
+    fill('#BBE9FF');
+    rect(dx,by,bw,bh);
+    image(down, dx, by);
+    fill('#B1AFFF');
+    rect(rx,by,bw,bh);
+    image(right, rx, by);
+    fill('#AEE8BB');
+    rect(gx,by,bw,bh);
+    
+     fill('#C5B3AA');
+    
+    
+    
+    print('order'+order);
+    
+    //mover
+    if(go==true){
+        if(frameCount%30==0 &&index<order.length){
+              mover();
+        }
+    }
+    ellipseMode(CORNER);
+    ellipse(mx,my,ms,ms);
+    print(mx + " y: "+my);
     
 }
 
@@ -93,29 +124,71 @@ function windowResized() {
 function mousePressed(){
      //collision detection
     if(mouseY>by&&mouseY<by+bh){
-        if(mouseX>ux &&mouseX<ux+bw){
-            //up
-            print('up pressed');
-            image(up, ax, bBarH/2);
-            ax+=aw;
-        }else if(mouseX>lx &&mouseX<lx+bw){
+     if(mouseX>lx &&mouseX<lx+bw){
             //left
             print('left pressed');
              image(left, ax, bBarH/2);
             ax+=aw;
+            order+='2';
+        }else if(mouseX>ux &&mouseX<ux+bw){
+            //up
+            print('up pressed');
+            image(up, ax, bBarH/2);
+            ax+=aw;
+            order+='1';
         }else if(mouseX>dx &&mouseX<dx+bw){
             //down
             print('down pressed');
              image(down, ax, bBarH/2);
             ax+=aw;
+            order+='3';
         }else if(mouseX>rx &&mouseX<rx+bw){
             //right
             print('right pressed');
              image(right, ax, bBarH/2);
            ax+=aw;
+            order+='4';
+            
+        }else if(mouseX>gx &&mouseX<gx+bw){
+           go=true;
         }
     }
 }
+
+function mover(){
+   let  i=order[index,index+1];
+    print("i="+i);
+    print(mx+' y:'+my);
+    if(i=='1'){
+          for(let n = 0; n<ss; n+=myv){
+             my-=myv;  
+            //  ellipse(mx,my,ms,ms);
+          } 
+        }else if(order[index,index+1]=='2'){
+            for(let n = 0; n<ss;n+=mxv){
+              mx-=mxv;
+          //      ellipse(mx,my,ms,ms);
+          }    
+        }else if(order[index,index+1]=='3'){
+          for(let n = 0; n<ss;n+=myv){
+            my+=myv;
+              //ellipse(mx,my,ms,ms);
+          }   
+        }else if(order[index,index+1]=='4'){
+            for(let n = 0; n<ss;n+=mxv){
+             mx+=mxv;
+              //  ellipse(mx,my,ms,ms);
+          }   
+        }
+    if(index<order.length){
+    index++;
+    }
+    }
+
+//#FFF7F8
+///#C2AFAE
+//#C5B3AA
+//#E6DEDB
 
 //python3 -m http.server
 //http://localhost:8000/
